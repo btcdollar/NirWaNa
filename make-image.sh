cf665730d7b5a6c455f8ae1cf719388fb56017f8
Daniel <163345545+btcdollar@users.noreply.github.com>
2025-05-03 00:12:06 +0700

    Update make-image.sh

--- make-image.sh
+++ make-image.sh
@@ -87,7 +87,7 @@ handle_profile_packages() {
     if [ "$1" == "rpi-4" ]; then
         PACKAGES+=" kmod-i2c-bcm2835 i2c-tools kmod-i2c-core kmod-i2c-gpio"
     elif [ "$ARCH_2" == "x86_64" ]; then
-        PACKAGES+=" kmod-iwlwifi kmod-ath10k iw-full pciutils ath10k-firmware-qca9377"
+        PACKAGES+=" kmod-iwlwifi kmod-ath10k iw-full pciutils ath10k-firmware-qca9377 openssh-sftp-server"
     fi
 
     case "${TYPE}" in
